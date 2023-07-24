d979d02ccc48051d232b2b7309a6d9f31a88c860
Antonio Llamas <antonio.llamas@thebridge.tech>
2023-07-24 08:44:49 +0200

    add helmet and compression

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -2,9 +2,14 @@ require('express-async-errors')
 const { json } = require('express')
 const morgan = require('morgan')
 
+const helmet = require('helmet')
+const compression = require('compression')
+
 const errors = require('../middlewares/errors')
 
 module.exports = function (app) {
+	app.use(helmet())
+	app.use(compression())
 	app.use(json())
 	app.use(morgan('tiny'))
 
